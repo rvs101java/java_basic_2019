a62d20a528e560c6abc752dd27e37038f5cc8018
rvs101 <raulvelasalas@gmail.com>
2019-03-05 01:45:18 +0100

    Añadir Archivos

---
+++ 12 - Estructura repetitiva do while/src/T13EjPromedio2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+/**
+ * Realizar un programa que permita ingresar el peso (en kilogramos) de piezas.
+ * 
+ * El proceso termina cuando ingresamos el valor 0. Se debe informar:
+ * 
+ * a) Cuántas piezas tienen un peso entre 9.8 Kg. y 10.2 Kg.?, cuántas con más
+ * de 10.2 Kg.? y cuántas con menos de 9.8 Kg.?
+ * 
+ * b) La cantidad total de piezas procesadas.
+ * 
+ * @author Rad
+ *
+ */
+public class T13EjPromedio2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce una medida entre 9.8 y 10.2 kg");
+		double medida = sc.nextDouble();
+		int pieza9k = 0;
+		int pieza10k = 0;
+
+		do {
+			if (((medida >= 9.8) && (medida <= 10.2) ? true : false)) {
+				if ((medida >= 9.8) && (medida <= 10.2) ? true : false) {
+
+				}
+
+			}
+
+		} while (medida != 0);
+
+	}
+}
