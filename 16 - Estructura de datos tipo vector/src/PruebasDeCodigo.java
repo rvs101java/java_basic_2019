c7b7fd3dda4f2ff6d6f53a7ae62e144386a54600
rvs101 <raulvelasalas@gmail.com>
2019-03-06 13:37:03 +0100

    16 Estructura de datos de tipo vector

---
+++ 16 - Estructura de datos tipo vector/src/PruebasDeCodigo.java
@@ -0,0 +1,14 @@
+
+public class PruebasDeCodigo {
+
+	public static void main(String[] args) {
+
+		System.out.println((float) (Math.random() * (0.6 + 0.9) + 0.60));
+		System.out.println((float) (Math.random() * (0.6 + 0.9) + 0.60));
+		System.out.println((float) (Math.random() * (0.6 + 0.9) + 0.60));
+		System.out.println((float) (Math.random() * (0.6 + 0.9) + 0.60));
+		System.out.println((float) (Math.random() * (0.6 + 0.9) + 0.60));
+		System.out.println((float) (Math.random() * (0.6 + 0.9) + 0.60));
+		System.out.println((float) (Math.random() * (0.6 + 0.9) + 0.60));
+	}
+}
