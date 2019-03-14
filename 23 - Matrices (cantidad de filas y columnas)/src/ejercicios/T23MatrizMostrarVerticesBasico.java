34ab2ccb9262c2e5f8accbca0fc1a91f3182bdba
rvs101 <raulvelasalas@gmail.com>
2019-03-14 17:24:05 +0100

    T23 añadir elementos

--- 23 - Matrices (cantidad de filas y columnas)/src/ejercicios/T23MatrizMostrarVerticesBasico.java
+++ 23 - Matrices (cantidad de filas y columnas)/src/ejercicios/T23MatrizMostrarVerticesBasico.java
@@ -1,19 +1,33 @@
 package ejercicios;
 
-public class T23Pruebas {
-	
-	private int[][] matriz;
+public class T23MatrizMostrarVerticesBasico {
+
+	public static void main(String[] args) {
+		T23MatrizMostrarVerticesBasico tp = new T23MatrizMostrarVerticesBasico();
+		tp.getVertices();
+	}
+
+	private int[][] matriz = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
 	private int primeraFila = 0;
 	private int primeraColum = 0;
-	
+
+	public T23MatrizMostrarVerticesBasico() {
+		for (int f = 0; f < getMatriz().length; f++) {
+			for (int c = 0; c < getMatriz()[f].length; c++) {
+				System.out.print(getMatriz()[f][c] + " ");
+			}
+			System.out.println();
+		}
+	}
+
 	public int getPrimeraFila() {
 		return primeraFila;
 	}
-	
+
 	public int[][] getMatriz() {
 		return matriz;
 	}
-	
+
 	public int getUltimaCol() {
 		return (getMatriz()[0].length - 1);
 	}
@@ -21,7 +35,7 @@ public class T23Pruebas {
 	public int getPrimeraColm() {
 		return primeraColum;
 	}
-	
+
 	public void getVertices() {
 		System.out.println("----------");
 		for (int f = getPrimeraFila(); f < getMatriz().length; f++) {
@@ -50,8 +64,4 @@ public class T23Pruebas {
 		}
 	}
 
-	public static void main(String[] args) {
-		
-	}
 }
-
